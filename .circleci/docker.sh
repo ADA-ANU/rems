a95a5e84ead1931cb12aa03bf57031e22da97c33
Adam Zammit <adam.zammit@acspri.org.au>
2023-08-02 10:08:05 +1000

    Update CICD to build on stage and test tags

--- .circleci/docker.sh
+++ .circleci/docker.sh
@@ -6,12 +6,18 @@ build_type=${1}
 if [ "${build_type}" == "circle" ] ; then
     tag1=circle
     tag2=$(git rev-parse HEAD)
+elif [ "${build_type}" == "test" ] ; then
+    tag1=test
+    tag2=$(git describe --tags --exact-match)
+elif [ "${build_type}" == "stage" ] ; then
+    tag1=stage
+    tag2=$(git describe --tags --exact-match)
 elif [ "${build_type}" == "release" ] ; then
     tag1=release
     tag2=$(git describe --tags --exact-match)
 else
-    echo "ERROR: Only \"circle\" and \"release\" arguments supported!"
-    echo "USAGE: ${0} [ circle | release ]"
+    echo "ERROR: Only \"circle\", \"test\", \"stage\" and \"release\" arguments supported!"
+    echo "USAGE: ${0} [ circle | release | stage | test ]"
     exit 1
 fi
 
