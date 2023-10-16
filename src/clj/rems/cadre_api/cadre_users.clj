5f225a5c0bad679c56530629325181a6ded9298b
Vikas Chinchansur <62231629+vikas-chinchansur@users.noreply.github.com>
2023-10-17 09:42:44 +1100

    Added new API to fetch the role of the logged-in user

---
+++ src/clj/rems/cadre_api/cadre_users.clj
@@ -0,0 +1,23 @@
+(ns rems.cadre-api.cadre-users
+  (:require [compojure.api.sweet :refer :all]
+            [rems.api.util] ; required for route :roles
+            [rems.db.cadredb.users :as users]
+            [rems.config :refer [env]]
+            [ring.util.http-response :refer :all]
+            [clojure.tools.logging :as log]
+            [rems.util :refer [get-user-id]]))
+
+(def cadre-users-api
+  (context "/cadre-users" []
+    :tags ["cadre-users"]
+
+    (GET "/roles" request
+      :summary "Get user roles of the current logged-in user"
+      :roles #{:logged-in}
+      (let [user-id (get-user-id)
+            response-json (users/fetch-logged-in-user-roles user-id)]
+        (when (:log-authentication-details env)
+          (log/info "response-json === " response-json))
+        {:status 200
+         :headers {"Content-Type" "application/json"}
+         :body response-json}))))
