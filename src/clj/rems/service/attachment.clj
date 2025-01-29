e8ba0150f391bd4ff4499e580fc5f3d75872aabd
MatejMitrev <143565846+MatejMitrev@users.noreply.github.com>
2025-01-29 15:05:32 +1100

    519 Fixing Forbidden Attachments for Two-Way Comments (#3)

--- src/clj/rems/service/attachment.clj
+++ src/clj/rems/service/attachment.clj
@@ -7,6 +7,7 @@
             [rems.auth.util :refer [throw-forbidden]]
             [rems.db.applications :as applications]
             [rems.db.attachments :as attachments]
+            [rems.db.cadredb.comments :as comments]
             [rems.util :refer [getx]]
             [ring.util.http-response :refer [ok content-type header]])
   (:import [java.io ByteArrayInputStream ByteArrayOutputStream]
@@ -16,6 +17,25 @@
   (-> (ok (ByteArrayInputStream. (:attachment/data attachment)))
       (header "Content-Disposition" (str "attachment;filename=" (pr-str (:attachment/filename attachment))))
       (content-type (:attachment/type attachment))))
+
+;; Where returned hashmap has the key of the attachment and the value of it's comment id
+(defn extract-comment-hashmap [seq-of-hmaps]
+  (reduce (fn [acc parent]
+            (let [parent-id (:id parent)
+                  attachments (:attachments parent)]
+              (reduce (fn [inner-acc attachment]
+                        (assoc inner-acc (:id attachment) parent-id))
+                      acc
+                      attachments)))
+          {}
+          seq-of-hmaps))
+
+;; Convert sequence of comments to hashmap where :id is the key and the value is the full comment itself
+(defn map-commentid-to-comment [seq-of-hmaps]
+  (reduce (fn [acc item]
+            (assoc acc (:id item) item))
+          {}
+          seq-of-hmaps))
 
 (defn get-application-attachment [user-id attachment-id]
   (let [attachment (attachments/get-attachment attachment-id)]
@@ -30,11 +50,21 @@
       (let [application (applications/get-application-for-user user-id (:application/id attachment))
             application-attachment (->> (:application/attachments application)
                                         (find-first #(= attachment-id (:attachment/id %))))
-            redacted? (= :filename/redacted (:attachment/filename application-attachment))]
-        (if (some? application-attachment) ; user can see the attachment
-          (assoc-some attachment :attachment/filename (when redacted?
-                                                        "redacted"))
-          (throw-forbidden))))))
+            redacted? (= :filename/redacted (:attachment/filename application-attachment))
+            comments (comments/get-app-comments (:application/id application) user-id)
+            comment-id-hmap (->> (map-commentid-to-comment (:comments comments)))
+            comments-attachments-hmap (->> (extract-comment-hashmap (:comments comments)))]
+        (if (contains? comments-attachments-hmap attachment-id)
+          (let [target-comment (get comment-id-hmap (get comments-attachments-hmap attachment-id))]
+            (if (not (contains? target-comment :addressed_to))
+              (assoc-some attachment :attachment/filename (when redacted? "redacted")) ;; Return attachment
+              (if (= (get-in target-comment [:addressed_to :userid]) user-id)
+                (assoc-some attachment :attachment/filename (when redacted? "redacted")) ;; Return attachment
+                (throw-forbidden))))
+          (if (some? application-attachment) ; user can see the attachment
+            (assoc-some attachment :attachment/filename (when redacted?
+                                                          "redacted"))
+            (throw-forbidden)))))))
 
 (defn add-application-attachment [user-id application-id file]
   (attachments/check-size file)
