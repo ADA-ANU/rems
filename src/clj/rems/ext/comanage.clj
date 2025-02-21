2116beb3c4f44058651dc9342ca29636d6d78923
Adam Zammit <adam.zammit@acspri.org.au>
2025-02-21 14:53:25 +1100

    started work on orcid unlinking

--- src/clj/rems/ext/comanage.clj
+++ src/clj/rems/ext/comanage.clj
@@ -47,19 +47,44 @@
     (catch Exception e
       (log/error "Error invoking CoManage GET API - " "co_people.json :" (.getMessage e)))))
 
-
-(defn get-orcid-identifier
-  "Get CoManage identifier for a given copersonid"
+(defn get-org-identity-links
+  "Get CoManage organisation identiy links for a user"
   [copersonid config]
   (try
-    (let [url (str (getx config :comanage-registry-url) "/identifiers.json?coid=" (getx config :comanage-registry-coid) "&copersonid=" copersonid)
+    (let [url (str (getx config :comanage-registry-url) "/co_org_identity_links.json?coid=" (getx config :comanage-registry-coid) "&copersonid=" copersonid)
+          response (http/get url (merge +common-opts+ {:basic-auth [(getx config :comanage-core-api-userid) (getx config :comanage-core-api-key)]}))]
+      (if (= 200 (:status response))
+        (let [parsed-json (:body response)
+              orgs (:CoOrgIdentityLinks parsed-json)]
+          orgs)
+        (throw (ex-info "Non-200 status code returned: " {:response response}))))
+    (catch Exception e
+      (log/error "Error invoking CoManage GET API - " "co_org_identity_links.json :" (.getMessage e)))))
+
+(defn get-orcid-org-id
+  "Get CoManage organisation identity for a user if they have and orcid identifier"
+  [userid config]
+  (let [orgs (get-org-identity-links (get-person-id userid config) config)]
+    (some #(has-orcid-identifiers? %) orgs)))
+
+(defn has-orcid-dentifiers?
+  "Check if orcid identifier"
+  [org]
+  (get-orcid-identifiers (:OrgIdentityId org) config)
+)
+
+(defn get-orcid-identifiers
+  "Get orcid identifiers (if any) from the coidentityid"
+  [coidentityid config]
+  (try
+    (let [url (str (getx config :comanage-registry-url) "/identifiers.json?coid=" (getx config :comanage-registry-coid) "&coidentityid=" coidentityid)
           response (http/get url (merge +common-opts+ {:basic-auth [(getx config :comanage-core-api-userid) (getx config :comanage-core-api-key)]}))]
       (if (= 200 (:status response))
         (let [parsed-json (:body response)
               identifiers (:Identifiers parsed-json)
               orcids (filterv #(str/includes? (:Type %) "orcid") identifiers)
               first-identifier (first orcids)
-              id (:Id first-identifier)]
+              id (if (nil? first-identifier) nil coidentityid)]
           id)
         (throw (ex-info "Non-200 status code returned: " {:response response}))))
     (catch Exception e
